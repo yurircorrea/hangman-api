402f67096f65789719d6f8b2e21fdb8ea1199031
yuri.correa <yuri.correa@softplan.com.br>
2022-03-18 09:12:33 -0300

    feat(initial-commit)
    Initial Commit
    -Spring Clean Project

---
+++ src/test/java/com/dell/codingassessment/hangman/game/HangmanGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dell.codingassessment.hangman.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
